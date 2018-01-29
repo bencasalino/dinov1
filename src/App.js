3e3454e15e7bde29b2623d65565b85dd4ef45b84
Ben Casalino <bencasalino@gmail.com>
2018-01-29 16:05:38 -0700

    what the fuckkkkkkk

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import JobForm from "./components/JobForm";
+import JobList from "./components/JobList";
 
 class App extends Component {
+  state = { jobs: [] };
+
+  componentDidMount() {
+    this.getJobs();
+  }
+
+  getJobs = () => {
+    return fetch("./listings.json")
+      .then(response => response.json())
+      .then(jobs => this.setState({ jobs }));
+  };
+
+  createJob = event => {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    const { jobs } = this.state;
+    jobs.unshift({
+      title: data.get("title"),
+      pay: data.get("pay"),
+      description: data.get("description"),
+      interested: []
+    });
+    this.setState({ jobs });
+    event.target.reset();
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <main>
+        <Header />
+        <section>
+          <h2>Job Listings</h2>
+          <ul id="job-listings">
+            <JobList jobs={this.state.jobs} />
+          </ul>
+        </section>
+        <aside id="side-bar">
+          <h3>Add a Job</h3>
+          <JobForm createJob={this.createJob} />
+        </aside>
+        <Footer />
+      </main>
     );
   }
 }
